1a8a75f425d5f18ee98480910071f3c2ba0e370b
Vladislav Knyshov <inova136@gmail.com>
2023-03-11 22:34:20 +0300

    add guestOnly prop to Protect component

--- src/components/Protect.jsx
+++ src/components/Protect.jsx
@@ -2,12 +2,16 @@ import { observable } from "mobx";
 import { Navigate } from "react-router-dom";
 import userStore from "../store/userStore";
 
-const Protect = observable(({ children, redirectTo }) => {
-  if (userStore.isAuthorized) {
-    return children;
+const Protect = observable((props) => {
+  const { children, redirectTo='/sign-in', guestOnly=false } = props;
+  const shouldRedirect = userStore.isAuthorized && !guestOnly 
+    || !userStore.isAuthorized && guestOnly;
+
+  if (shouldRedirect) {
+    return <Navigate to={redirectTo} replace />;
   }
 
-  return <Navigate to={redirectTo} replace />;
+  return children;
 });
 
 export default Protect;
