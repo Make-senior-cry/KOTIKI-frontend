47c27a4b62f7f6a5f0ab8b047b95938efd7dfc13
Vladislav Knyshov <inova136@gmail.com>
2023-03-11 23:02:06 +0300

    add routing frame

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-const App = () => null;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Protect from "./components/Protect";
+
+const App = () => <BrowserRouter>
+<Routes>
+  {/* Protected pages */}
+  <Route path="/profile" element={<Protect>Profile page<Route path="/edit">Update profile page</Route></Protect>} />
+  <Route path="/search-people" element={<Protect >Search people page</Protect>} />
+  <Route path="/create-post" element={<Protect>Create post page</Protect>} />
+  {/* Guest pages */}
+  <Route path="/sign-in" element={<Protect guestOnly redirectTo='/profile'>SignIn page</Protect>} />
+  <Route path="/sign-up" element={<Protect guestOnly redirectTo='/profile'>SignUp page</Protect>} />
+  {/* Main page */}
+  <Route path="/" element={<Protect>Feed page</Protect>}/>
+</Routes>
+</BrowserRouter>;
 
 export default App;
