58c6ce43a50dfbe2b2f8dae1564bc24610955e10
Vladislav Knyshov <inova136@gmail.com>
2023-03-11 23:01:03 +0300

    fix Protect component

--- src/components/Protect.jsx
+++ src/components/Protect.jsx
@@ -1,11 +1,11 @@
-import { observable } from "mobx";
+import { observer } from "mobx-react-lite";
 import { Navigate } from "react-router-dom";
 import userStore from "../store/userStore";
 
-const Protect = observable((props) => {
+const Protect = observer((props) => {
   const { children, redirectTo='/sign-in', guestOnly=false } = props;
-  const shouldRedirect = userStore.isAuthorized && !guestOnly 
-    || !userStore.isAuthorized && guestOnly;
+  const shouldRedirect = !userStore.isAuthorized && !guestOnly 
+    || userStore.isAuthorized && guestOnly;
 
   if (shouldRedirect) {
     return <Navigate to={redirectTo} replace />;
