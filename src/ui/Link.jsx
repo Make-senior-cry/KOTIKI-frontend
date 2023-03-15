4245793028689d56ffca32a818ef037cf464e0f3
kchaumeow <atrofimova516@gmail.com>
2023-03-15 20:14:28 +0300

    use cn fulction in link

--- src/ui/Link.jsx
+++ src/ui/Link.jsx
@@ -1,6 +1,7 @@
 import { Link as ReactRouterLink } from "react-router-dom";
+import cn from "classnames";
 export default function Link(props){
     return(
-        <ReactRouterLink to={props.href} className={`link-primary ${props.disableUnderline ? 'text-decoration-none' : ''}`} >{props.children}</ReactRouterLink>
+        <ReactRouterLink to={props.href} className={cn('link-primary', props.disableUnderline && 'text-decoration-none')} >{props.children}</ReactRouterLink>
     );
 }
