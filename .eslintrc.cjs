7f732643ad351002c2604ba4b92b3eb4d1a7f34c
kchaumeow <atrofimova516@gmail.com>
2023-03-16 21:51:21 +0300

    fixed eslintrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,37 @@
-
 const disableA11yRules = Object
-    .keys(require('eslint-plugin-jsx-a11y').rules)
-    .reduce((acc, rule) => { 
-        acc[`jsx-a11y/${rule}`] = 'off'; 
-        return acc 
-    }, {});
+  .keys(require('eslint-plugin-jsx-a11y').rules)
+  .reduce((acc, rule) => {
+    acc[`jsx-a11y/${rule}`] = 'off';
+    return acc;
+  }, {});
 
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  overrides: [
+    {
+      files: ['*.jsx', '*.js'],
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    "overrides": [
-        {
-            "files": ["*.jsx", "*.js"]
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "no-shadow": ["off"],
-        "import/no-extraneous-dependencies": ["off"],
-        "react/react-in-jsx-scope": ["off"],
-        "linebreak-style": ['off'],
-        "react/prop-types": ['off'],
-        ...disableA11yRules
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-shadow': ['off'],
+    'import/no-extraneous-dependencies': ['off'],
+    'react/react-in-jsx-scope': ['off'],
+    'linebreak-style': ['off'],
+    'react/prop-types': ['off'],
+    ...disableA11yRules,
+  },
+};
