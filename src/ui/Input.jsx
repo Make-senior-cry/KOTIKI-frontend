41f2254bffd16cb3dfdedfa4de571ff25b69747f
temerk1n <nikita.verigin23@gmail.com>
2023-03-19 16:26:12 +0300

    refactor: input and textarea moved to a variable

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,65 +1,49 @@
 import cn from 'classnames';
 import { useState } from 'react';
 
-function Form(
+function Input(
   {
-    label = '', textarea, placeholder, type = 'text',
+    label = '', textarea = false, placeholder, type = 'text',
   },
 ) {
   const [value, setValue] = useState('');
 
-  if (textarea) {
-    if (label) {
-      return (
-        <div>
-          <label className={cn('form-label')}>
-            { label }
-            <textarea
-              value={value}
-              placeholder={placeholder}
-              onChange={(event) => setValue(event.target.value)}
-            />
-          </label>
-        </div>
-      );
-    }
-    return (
-      <div>
+  const inp = (
+    textarea
+      ? (
         <textarea
           value={value}
           placeholder={placeholder}
           onChange={(event) => setValue(event.target.value)}
+          className={cn('form-control')}
         />
-      </div>
-    );
-  }
+      )
+      : (
+        <input
+          type={type}
+          value={value}
+          placeholder={placeholder}
+          className={cn('form-control')}
+          onChange={(event) => setValue(event.target.value)}
+        />
+      )
+  );
+
   if (label) {
     return (
       <div>
-        <label className={cn('form-label')}>
-          {label}
-          <input
-            type={type}
-            value={value}
-            placeholder={placeholder}
-            className={cn('form-control')}
-            onChange={(event) => setValue(event.target.value)}
-          />
+        <label className="form-label">
+          { label }
+          { inp }
         </label>
       </div>
     );
   }
   return (
     <div>
-      <input
-        type={type}
-        value={value}
-        placeholder={placeholder}
-        className={cn('form-control')}
-        onChange={(event) => setValue(event.target.value)}
-      />
+      { inp }
     </div>
   );
 }
 
-export default Form;
+export default Input;
