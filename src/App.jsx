f8b06d3f50376caee3f7e4c99a6a87ca9217323b
temerk1n <nikita.verigin23@gmail.com>
2023-03-19 22:49:55 +0300

    feat: added UserItem.jsx showcase

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Button from './ui/Button';
 import Link from './ui/Link';
 import Avatar from './ui/Avatar/Avatar';
 import ProfileHeader from './ui/ProfileHeader';
+import UserItem from './ui/UserItem';
 
 function App() {
   return (
@@ -44,6 +45,19 @@ function App() {
               <Avatar size="small" />
               <Button variant="secondary">Button</Button>
               <Link href="https://github.com/Make-senior-cry/KOTIKI-frontend" disableUnderline>Cats</Link>
+              <UserItem
+                user={{
+                  id: 1,
+                  name: 'Stupid Bastard',
+                  imageURL: 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80',
+                  description: 'bla bla bla',
+                  email: 'user@mail.ru',
+                  role: 'user',
+                  followersCount: 10,
+                  followingCount: 56,
+                  postsCount: 32,
+                }}
+              />
             </Protect>
 )}
         />
