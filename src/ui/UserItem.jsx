2ee12e6821375fab1c27a16f0609a15eaa8de964
temerk1n <nikita.verigin23@gmail.com>
2023-03-21 10:33:59 +0300

    feat: added border

--- src/ui/UserItem.jsx
+++ src/ui/UserItem.jsx
@@ -1,10 +1,12 @@
 import Avatar from './Avatar/Avatar';
 
-function UserItem({ user }) {
+function UserItem({ user, onClick }) {
   return (
-    <div className="container d-flex justify-content-start align-items-center">
-      <Avatar size="small" src={user.imageURL} />
-      <span className="ms-4">{user.name}</span>
+    <div className="container d-flex justify-content-start align-items-center text-primary border rounded p-2" onClick={onClick}>
+      <div className="ms-2">
+        <Avatar size="small" src={user.imageURL} />
+      </div>
+      <strong className="ms-4">{user.name}</strong>
       <span className="ms-5">{user.description}</span>
     </div>
   );
