ca1156046525f645e06c3afda95233cbfd510fb6
temerk1n <nikita.verigin23@gmail.com>
2023-03-22 17:25:30 +0300

    refactor: three dots vertical icon moved to separate file

--- src/ui/Post/Post.jsx
+++ src/ui/Post/Post.jsx
@@ -36,24 +36,13 @@ function Post({ post, onLike, onReport }) {
         { post.likesCount }
         <div className="dropdown float-end">
           <button
-            className="btn btn-secondary dropdown-toggle"
+            className="btn dropdown-toggle"
             type="button"
             id={dropdownMenuId}
             data-bs-toggle="dropdown"
             aria-expanded="false"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-three-dots-vertical"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"
-              />
-            </svg>
+            <img src="public/three-dots-vertical.svg" alt="" />
           </button>
           <ul className="dropdown-menu" aria-labelledby={dropdownMenuId}>
             <li>
