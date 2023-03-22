7945dfd610a0207ea6a796f7c6ba54f3b1b029b8
Vladislav Knyshov <inova136@gmail.com>
2023-03-22 21:51:05 +0300

    add LoginPage

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import AuthLayout from '../components/AuthLayout';
+import LoginForm from '../components/LoginForm';
+import userStore from '../store/userStore';
+
+export default function LoginPage() {
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLoginAccount = (email, password) => userStore
+    .signIn(email, password)
+    .catch((error) => setErrorMessage(error.message));
+
+  const handleLoginWithGoogle = () => {
+    alert('Не поддерживается');
+  };
+
+  return (
+    <AuthLayout errorMessage={errorMessage}>
+      <LoginForm
+        onLoginAccount={handleLoginAccount}
+        onLoginWithGoogle={handleLoginWithGoogle}
+      />
+    </AuthLayout>
+  );
+}
