00fa5ab4ace70175d716913e925e49f10dbeb21c
Vladislav Knyshov <inova136@gmail.com>
2023-04-12 11:44:55 +0300

    add printing config in dev environment

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './global.css';
+import config from './config/config';
+
+if (import.meta.env.MODE !== 'production') {
+  console.table(config);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
