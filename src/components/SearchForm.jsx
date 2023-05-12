ec16faf46c4f95a3c5305b40c2a79df5c0797755
temerk1n <nikita.verigin23@gmail.com>
2023-05-12 23:55:04 +0300

    feat: added form for search users

---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import Input from '../ui/Input';
+import Button from '../ui/Button';
+
+export default function SearchForm({ onSearchUsers, searchValue }) {
+  const [value, setValue] = useState(searchValue);
+
+  function handleSearchPeople(event) {
+    event.preventDefault();
+    onSearchUsers(value);
+  }
+
+  return (
+    <form onSubmit={handleSearchPeople} className="row g-3">
+      <div className="col-auto">
+        <Input
+          placeholder="Имя или email"
+          type="text"
+          required
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+      <div className="col-auto">
+        <Button type="submit">Найти</Button>
+      </div>
+    </form>
+  );
+}
