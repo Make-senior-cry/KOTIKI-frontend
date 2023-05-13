37194cfa98ff93d20e50fed7d3852fc29a0ab3d0
temerk1n <nikita.verigin23@gmail.com>
2023-05-13 17:14:51 +0300

    chore: handling network errors

--- src/pages/SearchPeoplePage.jsx
+++ src/pages/SearchPeoplePage.jsx
@@ -1,28 +1,27 @@
 import { useEffect, useState } from 'react';
 import SearchForm from '../components/SearchForm';
 import UserItemList from '../ui/UserItemList';
-import Navbar from '../components/Navbar/Navbar';
 import useSearchValue from '../hooks/useSearchValue';
 import searchUsers from '../api/searchUsers';
+import DefaultLayout from '../ui/DefaultLayout';
 
 export default function SearchPeoplePage() {
   const [users, setUsers] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
   const [searchValue, setSearchValue] = useSearchValue();
 
   useEffect(() => {
     searchUsers(searchValue, 0, 10)
-      .then((response) => {
-        setUsers(response.dataList);
-      });
+      .then((response) => setUsers(response.dataList))
+      .catch((error) => setErrorMessage(error.message));
   }, [searchValue]);
 
   return (
-    <main className="container">
-      <Navbar />
-      <div className="container d-flex flex-column gap-3 pt-3">
-        <SearchForm onSearchUsers={setSearchValue} searchValue={searchValue} />
-        <UserItemList users={users} />
-      </div>
-    </main>
+    <DefaultLayout
+      hero={<SearchForm onSearchUsers={setSearchValue} searchValue={searchValue} />}
+      errorMessage={errorMessage}
+    >
+      <UserItemList users={users} />
+    </DefaultLayout>
   );
 }
