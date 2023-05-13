8cee327808a0de7c5bd4b11294b84c7cda60306e
temerk1n <nikita.verigin23@gmail.com>
2023-05-13 22:18:18 +0300

    fix: sync request with backend

--- src/api/getPosts.js
+++ src/api/getPosts.js
@@ -3,20 +3,21 @@ import PostRecord from '../entities/postRecord';
 import Pagination from '../entities/pagination';
 
 const getPosts = (userId, skip = 0, limit = 10) => httpClient
-  .get('/get-posts', {
-    paramas: {
-      userId,
+  .get('/post', {
+    params: {
+      user_id: userId,
       skip,
       limit,
     },
   })
   .then((response) => {
     const {
-      dataList, hasPrev, hasNext, skip, limit,
+      // eslint-disable-next-line no-unused-vars
+      posts, author, hasPrev, hasNext, skip, limit,
     } = response.data;
-    const posts = dataList.map((postData) => new PostRecord(postData));
+    const foundPosts = posts.map((postData) => new PostRecord(postData));
     return new Pagination({
-      dataList: posts,
+      dataList: foundPosts,
       hasPrev,
       hasNext,
       skip,
