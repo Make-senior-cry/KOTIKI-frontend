be9e2a318e6d08b82a746543df14e177ff97fd48
temerk1n <nikita.verigin23@gmail.com>
2023-05-13 12:02:55 +0300

    fix: query parameter

--- src/components/Navbar/NavbarSearch.jsx
+++ src/components/Navbar/NavbarSearch.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Input from '../../ui/Input';
+import useSearchValue from '../../hooks/useSearchValue';
 
 export default function NavbarSearch() {
   const navigate = useNavigate();
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchValue] = useSearchValue();
+  const [currentSearchValue, setCurrentSearchValue] = useState(searchValue);
 
   function onKeyDownEnter(event) {
     if (event.key === 'Enter') {
-      navigate(`/search?query=${searchQuery}`);
+      navigate(`/search/user?name=${currentSearchValue}`);
     }
   }
 
@@ -16,8 +18,8 @@ export default function NavbarSearch() {
     <Input
       placeholder="Поиск"
       onKeyDown={onKeyDownEnter}
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
+      value={currentSearchValue}
+      onChange={(e) => setCurrentSearchValue(e.target.value)}
     />
   );
 }
