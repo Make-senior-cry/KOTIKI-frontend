90f8afe9e58821d90cf9faca954bb009de6abf73
temerk1n <nikita.verigin23@gmail.com>
2023-05-14 15:31:44 +0300

    chore: added dev config

--- src/api/getPosts.js
+++ src/api/getPosts.js
@@ -1,6 +1,7 @@
 import httpClient from './httpClient';
 import PostRecord from '../entities/postRecord';
 import Pagination from '../entities/pagination';
+import config from '../config/config';
 
 const getPosts = (userId, skip = 0, limit = 10) => httpClient
   .get('/post', {
@@ -14,24 +15,29 @@ const getPosts = (userId, skip = 0, limit = 10) => httpClient
     const {
       posts, author, hasPrev, hasNext, skip, limit,
     } = response.data;
-    // const foundPosts = posts.map((postData) => {
-    //   const post = new PostRecord(postData);
-    //   post.author = author;
-    //   return post;
-    // });
-    const postExample = new PostRecord({
-      id: 1,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      imageURL: 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80',
-      createdAt: new Date(),
-      banned: false,
-      likesCount: 2,
-      reportsCount: 0,
-      author,
-    });
-    const postExample2 = JSON.parse(JSON.stringify(postExample));
-    postExample2.id = 2;
-    const foundPosts = [postExample, postExample2, ...posts];
+    let foundPosts;
+    if (config.DEV) {
+      foundPosts = posts.map((postData) => {
+        const post = new PostRecord(postData);
+        post.author = author;
+        return post;
+      });
+    } else {
+      const postExample = new PostRecord({
+        id: 1,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        imageURL: 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8&w=1000&q=80',
+        createdAt: new Date(),
+        banned: false,
+        likesCount: 2,
+        reportsCount: 0,
+        author,
+      });
+      const postExample2 = JSON.parse(JSON.stringify(postExample));
+      postExample2.id = 2;
+      foundPosts = [postExample, postExample2, ...posts];
+    }
+
     return new Pagination({
       dataList: foundPosts,
       hasPrev,
--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 const config = {
   API_URL: import.meta.env.VITE_APP_API_URL,
   FAKE_LOGIN: import.meta.env.VITE_APP_FAKE_LOGIN === 'true',
+  DEV: import.meta.env.VITE_APP_DEV === 'true',
 };
 
 export default config;
