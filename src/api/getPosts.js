e57712132f26cfa139d4a7f48b05484f81b66539
temerk1n <nikita.verigin23@gmail.com>
2023-05-14 18:36:47 +0300

    fix: config

--- src/api/getPosts.js
+++ src/api/getPosts.js
@@ -17,7 +17,7 @@ const getPosts = (userId, skip = 0, limit = 10) => httpClient
     } = response.data;
 
     let foundPosts;
-    if (config.DEV) {
+    if (config.MODE === 'development') {
       const postExample = new PostRecord({
         id: 1,
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   API_URL: import.meta.env.VITE_APP_API_URL,
   FAKE_LOGIN: import.meta.env.VITE_APP_FAKE_LOGIN === 'true',
-  DEV: import.meta.env.VITE_APP_DEV === 'true',
+  MODE: import.meta.env.MODE,
 };
 
 export default config;
