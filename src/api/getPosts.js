d012bc63c74a1f835ea4ab7820179be7a3ee5231
temerk1n <nikita.verigin23@gmail.com>
2023-05-14 15:33:20 +0300

    fix

--- src/api/getPosts.js
+++ src/api/getPosts.js
@@ -15,14 +15,9 @@ const getPosts = (userId, skip = 0, limit = 10) => httpClient
     const {
       posts, author, hasPrev, hasNext, skip, limit,
     } = response.data;
+
     let foundPosts;
     if (config.DEV) {
-      foundPosts = posts.map((postData) => {
-        const post = new PostRecord(postData);
-        post.author = author;
-        return post;
-      });
-    } else {
       const postExample = new PostRecord({
         id: 1,
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
@@ -36,6 +31,12 @@ const getPosts = (userId, skip = 0, limit = 10) => httpClient
       const postExample2 = JSON.parse(JSON.stringify(postExample));
       postExample2.id = 2;
       foundPosts = [postExample, postExample2, ...posts];
+    } else {
+      foundPosts = posts.map((postData) => {
+        const post = new PostRecord(postData);
+        post.author = author;
+        return post;
+      });
     }
 
     return new Pagination({
