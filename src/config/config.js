b121c5b70c97bbca7c68ce8f6591dbb50f6c1d14
temerk1n <nikita.verigin23@gmail.com>
2023-05-15 20:14:27 +0300

    fix: env variable

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   API_URL: import.meta.env.VITE_APP_API_URL,
   FAKE_LOGIN: import.meta.env.VITE_APP_FAKE_LOGIN === 'true',
-  MODE: import.meta.env.MODE,
+  FAKE_POSTS: import.meta.env.VITE_APP_FAKE_POSTS === 'true',
 };
 
 export default config;
