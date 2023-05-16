507b05ba80f84ad2331a7618d18f1c6794552bde
temerk1n <nikita.verigin23@gmail.com>
2023-05-16 20:18:10 +0300

    fix: correct date display

--- src/ui/Post/PostTitle.jsx
+++ src/ui/Post/PostTitle.jsx
@@ -5,15 +5,19 @@ function PostTitle({
   avatar, authorName, createdAt, authorLink,
 }) {
   return (
-    <div className="card-title d-flex gap-2 align-items-center mb-4">
-      <ReactRouterLink
-        to={authorLink}
-        className="d-flex gap-2 align-items-center text-black text-decoration-none"
-      >
-        <Avatar src={avatar} size="small" />
-        <span>{ authorName }</span>
-      </ReactRouterLink>
-      <span>{ createdAt }</span>
+    <div className="card-title d-flex gap-2 align-items-center justify-content-between mb-4">
+      <div>
+        <ReactRouterLink
+          to={authorLink}
+          className="d-flex gap-2 align-items-center text-black text-decoration-none"
+        >
+          <Avatar src={avatar} size="small" />
+          <span>{ authorName }</span>
+        </ReactRouterLink>
+      </div>
+      <span>
+        { createdAt }
+      </span>
     </div>
   );
 }
